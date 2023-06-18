8dc1248e40a96ab1d33a29e85ddd374b9fb6f9b6
artrudy <art.rudy@gmail.com>
2023-06-18 21:35:15 +0300

    counting function added

--- js/app.js
+++ js/app.js
@@ -4,16 +4,64 @@ class Calorietracker {
         this._totalcalorie = 0;
         this._meals = [];
         this._workouts = [];
+
+        this._displayCaloriesTotal();
+        this._displayCaloriesLimit();
+        this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();
+        this._displayCaloriesRemaining();
     }
+
+    //Public methods //API
 
     addMeals (meal){
         this._meals.push(meal);
         this._totalcalories += meal.calories;
+        this._render();
     }
 
     addWorkout (workout){
         this.workout.push(workout);
         this._totalcalories -= workout.calories;
+        this._render();
+    }
+
+
+    //Private methods 
+    _displayCaloriesTotal(){
+        const totalCaloriesEl = document.getElementById('calories-total');
+        totalCaloriesEl.innerHTML = this._totalcalories;
+    }
+
+    _displayCaloriesLimit(){
+        const calorieLimitEl = document.getElementById('calories-limit');
+        calorieLimitEl.innerHTML = this._calorieLimit;
+    }
+
+    _displayCaloriesConsumed(){
+        const caloriesConsumedEl = document.getElementById('calorie-consumed');
+        const consumed  = this._meals.reduce((total, meal) => total + meal.calories, 0);
+        caloriesConsumedEl.innerHTML = consumed;
+    }
+
+    _displayCaloriesBurned(){
+        const caloriesBurnedEl = document.getElementById('calorie-burned');
+        const burned  = this.workout.reduce((total, workout) => total + workout.calories, 0);
+        caloriesBurnedEl.innerHTML = burned;
+    }
+
+    _displayCaloriesRemaining(){
+        const caloriesRemainingEl = document.getElementById('calories-remaining');
+        const remaining = this._calorieLimit - this._totalcalorie;
+        caloriesRemainingEl.innerHTML = remaining;
+
+    }
+
+    _render(){
+        this._displayCaloriesTotal();
+        this._displayCaloriesConsumed();
+        this._displayCaloriesBurned();  
+        this._displayCaloriesRemaining();
     }
 }
 class Workout {
